fd11118cc6bf68784c57ad9adfa10aa034c30d9b
hb23ecb0a41-hub <hb23ecb0a41@student.nitw.ac.in>
2026-07-02 11:58:54 +0530

    Create spectral_ip.cpp

---
+++ src/spectral_ip.cpp
@@ -0,0 +1,8 @@
+//=========================================================
+// SpectFormer-Small
+// Spectral IP Implementation
+//=========================================================
+
+#include "spectral_ip.h"
+
+// Actual implementation will be added in the next commit.
