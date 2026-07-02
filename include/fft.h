c84bc9b594445d238b10c08706e77725e0170530
hb23ecb0a41-hub <hb23ecb0a41@student.nitw.ac.in>
2026-07-02 12:06:04 +0530

    Update fft.h

--- include/fft.h
+++ include/fft.h
@@ -1,1 +1,18 @@
 
+#ifndef FFT_H
+#define FFT_H
+
+#include "types.h"
+#include "config.h"
+
+//--------------------------------------------------
+// 2D FFT for SpectFormer Spectral Block
+//--------------------------------------------------
+
+void FFT2D(
+    data_t input[14][14][EMBED_DIM],
+    data_t real_out[14][14][EMBED_DIM],
+    data_t imag_out[14][14][EMBED_DIM]
+);
+
+#endif
