ea925b814225b1d690959d7e5c6b2cce96a5699a
hb23ecb0a41-hub <hb23ecb0a41@student.nitw.ac.in>
2026-07-02 11:53:11 +0530

    Update spectral_ip.h

--- include/spectral_ip.h
+++ include/spectral_ip.h
@@ -1,1 +1,12 @@
 
+#ifndef SPECTRAL_IP_H
+#define SPECTRAL_IP_H
+
+#include "spectformer.h"
+
+void Spectral_IP(
+    data_t input[NUM_PATCHES][EMBED_DIM],
+    data_t output[NUM_PATCHES][EMBED_DIM]
+);
+
+#endif
