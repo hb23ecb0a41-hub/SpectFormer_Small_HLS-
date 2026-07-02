f62deeda90954aef763f4565a1a51beae7a19263
hb23ecb0a41-hub <hb23ecb0a41@student.nitw.ac.in>
2026-07-02 12:06:30 +0530

    Update fft.cpp

--- src/fft.cpp
+++ src/fft.cpp
@@ -1,1 +1,32 @@
 
+#include "fft.h"
+
+void FFT2D(
+    data_t input[14][14][EMBED_DIM],
+    data_t real_out[14][14][EMBED_DIM],
+    data_t imag_out[14][14][EMBED_DIM]
+)
+{
+
+#pragma HLS INLINE off
+
+ROW_LOOP:
+    for(int r=0; r<14; r++)
+    {
+COL_LOOP:
+        for(int c=0; c<14; c++)
+        {
+EMBED_LOOP:
+            for(int e=0; e<EMBED_DIM; e++)
+            {
+
+#pragma HLS PIPELINE II=1
+
+                real_out[r][c][e] = input[r][c][e];
+                imag_out[r][c][e] = 0;
+
+            }
+        }
+    }
+
+}
